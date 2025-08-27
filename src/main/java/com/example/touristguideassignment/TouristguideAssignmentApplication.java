42cd49b1de0a43350c5fe7873d0048f399aed31e
NikolajPedersen29 <mejlby2002@hotmail.com>
2025-08-27 09:16:46 +0200

    Started project

---
+++ src/main/java/com/example/touristguideassignment/TouristguideAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristguideassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristguideAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristguideAssignmentApplication.class, args);
+    }
+
+}
